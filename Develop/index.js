3d689c9f4ece92257cfdfa0d643bb0b705fa42d1
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2023-10-19 22:20:14 -0700

    added function init and init call. Questions populate in terminal but unable to access newly created files.

--- Develop/index.js
+++ Develop/index.js
@@ -3,63 +3,64 @@ const inquirer = require('inquirer');
 const fs = require("fs");
 const generateMarkdown = require('./utils/generateMarkdown');
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide: -What was your motivation? -Why did you build this project? -What problems does it solve? -What did you learn?',
-            name: 'Description'
-        },
-        {
-            type: 'input',
-            message: 'If your README is long, add a table of contents to make it as easy for users to find what they need.',
-            name: 'Table of Contents (Optional)'
-        },
-        {
-            type: 'input',
-            message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
-            name: 'Installation'
-        },
-        {
-            type: 'input',
-            message: 'Provide instructions and examples for use. Include screenshots as needed. To add a screenshot, create an assets/images folder in your repository and upload your screenshot to it.',
-            name: 'Usage'
-        },
-        {
-            type: 'input',
-            message: 'List your collaborators, if any, with links to their GitHub profiles. If you used any third-party assests that require attribution, list the creators with links to their primary web presence in this section. If you followed tutorials, include links to those here as well.',
-            name: 'Credits'
-        },
-        {
-            type: 'input',
-            message: 'The last section of a high-quality README file is the license. This lets other developers know what they can an cannot do with your project.',
-            name: 'License'
-        },
-        {
-            type: 'input',
-            message: '',
-            name: 'Badges'
-        },
-        {
-            type: 'input',
-            question: '',
-            name: 'Features'
-        },
-        {
-            type: 'input',
-            question: '',
-            name: 'How to Contribute'
-        },
-        {
-            type: 'input',
-            question: '',
-            name: 'Tests'
-        },
-    ])
-    .then((response)=>{
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide: -What was your motivation? -Why did you build this project? -What problems does it solve? -What did you learn?',
+                name: 'Description'
+            },
+            {
+                type: 'input',
+                message: 'If your README is long, add a table of contents to make it as easy for users to find what they need.',
+                name: 'Table of Contents (Optional)'
+            },
+            {
+                type: 'input',
+                message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+                name: 'Installation'
+            },
+            {
+                type: 'input',
+                message: 'Provide instructions and examples for use. Include screenshots as needed. To add a screenshot, create an assets/images folder in your repository and upload your screenshot to it.',
+                name: 'Usage'
+            },
+            {
+                type: 'input',
+                message: 'List your collaborators, if any, with links to their GitHub profiles. If you used any third-party assests that require attribution, list the creators with links to their primary web presence in this section. If you followed tutorials, include links to those here as well.',
+                name: 'Credits'
+            },
+            {
+                type: 'input',
+                message: 'The last section of a high-quality README file is the license. This lets other developers know what they can an cannot do with your project.',
+                name: 'License'
+            },
+            {
+                type: 'input',
+                message: '',
+                name: 'Badges'
+            },
+            {
+                type: 'input',
+                question: '',
+                name: 'Features'
+            },
+            {
+                type: 'input',
+                question: '',
+                name: 'How to Contribute'
+            },
+            {
+                type: 'input',
+                question: '',
+                name: 'Tests'
+            },
+        ])
+        .then((response) => {
 
-        const markdown = generateMarkdown(response);
-        const html = `
+            const markdown = generateMarkdown(response);
+            const html = `
         <!DOCTYPE html>
         <html lang="en">
         <head>
@@ -95,23 +96,25 @@ inquirer
         </body>
         </html> `;
 
-        fs.writeFile('README.html', html, (err) => {
-            if (err) {
-                console.error(err);
-            } else {
-                console.log('HTML file created successfully!');
-            }    
-        });
+            fs.writeFile('README.html', html, (err) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log('HTML file created successfully!');
+                }
+            });
 
-        fs.writeFile('README.md', markdown, (err) => {
-            if (err) {
-                console.error(err);
-            } else {
-                console.log('Markdown file created successfully!');
-            }
-        } );    
-    });
-    
+            fs.writeFile('README.md', markdown, (err) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log('Markdown file created successfully!');
+                }
+            });
+        });
+}
+init();
+
 
 // // TODO: Create an array of questions for user input
 // const questions = [];
@@ -123,11 +126,7 @@ inquirer
 // function writeToFile(fileName, data) { }
 // // use FS module to write data to readme file, fileName
 
-// // TODO: Create a function to initialize app
-// function init() { }
 
-// // Function call to initialize app
-// init();
 
 
 //Pseudocode
