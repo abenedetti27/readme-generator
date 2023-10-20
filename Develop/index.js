9458e6c86e0ed5e664f7a1701f68a6d9d7d4e955
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2023-10-19 18:49:46 -0700

    add pseudocode from session with James

---
+++ Develop/index.js
@@ -0,0 +1,81 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require("fs");
+const generateMarkdown = require('./utils/generateMarkdown');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'Prove a short description explaining the what, why, and how of your project. Use the following questions as a guide: -What was your motivation? -Why did you build this project? -What problems does it solve? -What did you learn?',
+            name: 'Description'
+        },
+        {
+            type: 'input',
+            message: 'If your README is long, add a table of contents to make it as easy for users to find what they need.',
+            name: 'Table of Contents (Optional)'
+        },
+        {
+            type: 'input',
+            message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+            name: 'Installation'
+        },
+        {
+            type: 'input',
+            message: 'Provide instructions and examples for use. Include screenshots as needed. To add a screenshot, create an assets/images folder in your repository and upload your screenshot to it',
+            name: 'Usage'
+        },
+        {
+            type: 'input',
+            message: '',
+            name: 'Credits'
+        },
+        {
+            type: 'input',
+            message: '',
+            name: 'License'
+        },
+        {
+            type: 'input',
+            message: '',
+            name: 'Badges'
+        },
+        {
+            type: 'input',
+            question: '',
+            name: 'Features'
+        },
+        {
+            type: 'input',
+            question: '',
+            name: 'How to Contribute'
+        },
+        {
+            type: 'input',
+            question: '',
+            name: 'Tests'
+        },
+    ])
+
+// TODO: Create an array of questions for user input
+const questions = [];
+//* add type, name, and message for each question
+//* add questions: add readme sections per acceptance criteria (desctiption, usage, license, etc)
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) { }
+// use FS module to write data to readme file, fileName
+
+// TODO: Create a function to initialize app
+function init() { }
+
+// Function call to initialize app
+init();
+
+
+//Pseudocode
+//use inquirer to display questions to user
+//call function generateMarkdown
+//take what is returned and pass to writeToFile function
+
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,35 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  // returns a string that is a link (physical link to site that hosts license)
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  // create a conditional to check if license exists
+  // call functions renderlicenseBadge, renderLicenseLink
+  // use template literal to format license section and call functions
+  // return template literal
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  //plug in data to template literals
+  //data is the layout of what the ultimate readme file will generate
+  // hint- call renderLicense function inside this function
+
+  return `# ${data.title}
+//write entire readme file with subheadings
+`;
+}
+
+module.exports = generateMarkdown;
+
+
+//create a conditional statement to check if the license exists
+  //if license exists, 
